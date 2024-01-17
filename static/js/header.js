1773a7dd9cca3683774a0cc4278ae055559a9180
NDDarrow <shwo5158@naver.com>
2024-01-17 17:23:58 +0900

    0523

---
+++ static/js/header.js
@@ -0,0 +1,16 @@
+
+
+function searchOpen(){
+    var modal = document.getElementById("search_modal")
+    console.log(modal)
+    modal.style.display='block'
+};
+function search(){
+    alert("미구현입니다")
+    location.reload(true);
+};
+function searchClose(){
+    var MC = document.getElementById("search_modal")
+    console.log(MC)
+    MC.style.display='none'
+};
